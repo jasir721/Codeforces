016e261c2dd39078de9443629b92fb31e949b422
Jasir K <jasirkadarsha721091@gmail.com>
2021-01-27 11:12:00 +0530

    Add solution for problem `A - Olesya and Rodion` Link: https://codeforces.com/contest/584/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/584/submission/105535889

---
+++ codeforces/584/A.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fr(i,n) for(int i=0;i<n;i++)
+int gcd(int a,int b) { if (a == 0) return b; return gcd(b%a, a);}
+int maxi(int a[],int n)
+{
+   int max=a[0],j=0;
+   for(int i=1;i<n;i++)
+   {
+      if(a[i]>max)
+      {
+         max=a[i];
+         j=i;
+      }
+   }
+   return j;
+}
+int mini(int a[],int n)
+{
+   int min=a[0],j=0;
+   for(int i=1;i<n;i++)
+   {
+      if(a[i]<min)
+      {
+         min=a[i];
+         j=i;
+      }
+   }
+   return j;
+}
+/*********************************************************************************************************************/
+void solve()
+{   
+   int n,t;
+   cin>>n>>t;
+   if(n==1&&t==10)
+   cout<<-1;
+   else if(t==10)
+   {
+      cout<<1;
+      fr(i,n-1)
+      cout<<0;
+   }
+   else
+   {
+      fr(i,n)
+      cout<<t;
+   }
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+        solve();
+    return 0;    
+}
