dab23abfa74fc19323f733703e31dddaf243c510
Jasir K <jasirkadarsha721091@gmail.com>
2021-01-27 17:31:00 +0530

    Add solution for problem `A - Mahmoud and Ehab and the even-odd game` Link: https://codeforces.com/contest/959/problem/A Tags: games, math, *800 Ref: https://codeforces.com/contest/959/submission/105568235

---
+++ codeforces/959/A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fs ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define mod 1000000007
+#define fr(i,n) for(i=0;i<n;i++)
+#define frl(i,n) for(i=n;i>0;i--)
+int gcd(int a,int b) 
+{ 
+	if (a == 0) 
+	return b; 
+	return gcd(b%a, a);
+}
+
+int main()
+{
+    fs 
+    ll i,t,j;
+    cin>>t;
+    //ll k=0;
+    
+        
+            if(t%2==0)
+            cout<<"Mahmoud";
+            else
+            cout<<"Ehab";
+            
+            
+        
+    
+    return 0;
+}
