6c33aa722685d5b39c6098cb34d5fdace8add63d
Jasir K <jasirkadarsha721091@gmail.com>
2021-01-28 10:10:00 +0530

    Add solution for problem `A - Laptops` Link: https://codeforces.com/contest/456/problem/A Tags: sortings, *1100 Ref: https://codeforces.com/contest/456/submission/105630644

---
+++ codeforces/456/A.cpp
@@ -0,0 +1,64 @@
+   #include<bits/stdc++.h>
+   using namespace std;
+   #define ll long long
+   #define fr(i,n) for(int i=0;i<n;i++)
+   int gcd(int a,int b) { if (a == 0) return b; return gcd(b%a, a);}
+   int maxi(int a[],int n)
+   {
+      int max=a[0],j=0;
+      for(int i=1;i<n;i++)
+      {
+         if(a[i]>max)
+         {
+            max=a[i];
+            j=i;
+         }
+      }
+      return j;
+   }
+   int mini(int a[],int n)
+   {
+      int min=a[0],j=0;
+      for(int i=1;i<n;i++)
+      {
+         if(a[i]<min)
+         {
+            min=a[i];
+            j=i;
+         }
+      }
+      return j;
+   }
+   /*********************************************************************************************************************/
+   void solve()
+   {   
+      int n,k;
+      cin>>n;
+      int a[n];
+      map<int,int>v;
+      fr(i,n)
+      {
+      cin>>a[i]>>k;
+      v[a[i]]=k;
+      }
+      sort(a,a+n);
+      fr(i,n-1)
+      {
+          if(a[i]<a[i+1])
+          {
+              if(v[a[i]]>v[a[i+1]])
+              {
+                  cout<<"Happy Alex\n";
+                  return;
+              }
+          }
+      }
+      cout<<"Poor Alex\n";
+   }
+   int main()
+   {
+      ios::sync_with_stdio(0);
+      cin.tie(0);
+         solve();
+      return 0;    
+   }
