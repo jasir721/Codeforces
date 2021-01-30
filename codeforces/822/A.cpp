fb3bc4cd0e5cb52f08087f8981c642fbce1d4593
Jasir K <jasirkadarsha721091@gmail.com>
2021-01-30 18:23:00 +0530

    Add solution for problem `A - I'm bored with life` Link: https://codeforces.com/contest/822/problem/A Tags: implementation, math, number theory, *800 Ref: https://codeforces.com/contest/822/submission/106020984

---
+++ codeforces/822/A.cpp
@@ -0,0 +1,56 @@
+   #include<bits/stdc++.h>
+   using namespace std;
+   #define ll long long
+   #define fr(i,n) for(int i=0;i<n;i++)
+   int gcd(int a,int b) { if (a == 0) return b; return gcd(b%a, a);}
+   int maxi(int a[],int n)
+   {
+      int max=a[0],j=0;
+      for(int i=1;i<n;i++)
+      {
+         if(a[i]>max)
+         {
+            max=a[i];
+            j=i;
+         }
+      }
+      return j;
+   }
+   int mini(int a[],int n)
+   {
+      int min=a[0],j=0;
+      for(int i=1;i<n;i++)
+      {
+         if(a[i]<min)
+         {
+            min=a[i];
+            j=i;
+         }
+      }
+      return j;
+   }
+   /*********************************************************************************************************************/
+   ll fact(ll n)
+   {
+      ll k=1;
+      while(n!=0)
+      {
+         k*=(n);
+         n--;
+      }
+      return k;
+   }
+   void solve()
+   {   
+      ll n,m;
+      cin>>n>>m;
+      ll k=fact(min(n,m));
+      cout<<k;
+   }
+   int main()
+   {
+      ios::sync_with_stdio(0);
+      cin.tie(0);
+         solve();
+      return 0;    
+   }
