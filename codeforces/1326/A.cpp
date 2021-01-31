94e689cca03b6ab0c861ed779b7c95779b85b2cc
Jasir K <jasirkadarsha721091@gmail.com>
2021-01-31 10:22:00 +0530

    Add solution for problem `A - Bad Ugly Numbers` Link: https://codeforces.com/contest/1326/problem/A Tags: constructive algorithms, number theory, *1000 Ref: https://codeforces.com/contest/1326/submission/106070874

---
+++ codeforces/1326/A.cpp
@@ -0,0 +1,59 @@
+   #include<bits/stdc++.h>
+   using namespace std;
+   #define ll long long
+   #define fr(i,n) for(int i=0;i<n;i++)
+   int gcd(int a,int b) { if (a == 0) return b; return gcd(b%a, a);}
+   int maxi(int a[],int n)
+   {
+      int max=a[0],j=0;
+      for(int i=1;i<n;i++)
+      {
+         if(a[i]>max)
+         {
+            max=a[i];
+            j=i;
+         }
+      }
+      return j;
+   }
+   int mini(int a[],int n)
+   {
+      int min=a[0],j=0;
+      for(int i=1;i<n;i++)
+      {
+         if(a[i]<min)
+         {
+            min=a[i];
+            j=i;
+         }
+      }
+      return j;
+   }
+   /*********************************************************************************************************************/
+   void solve()
+   {   
+      int n;
+      cin>>n;
+      if(n==1)
+      {
+         cout<<-1<<"\n";
+         return;
+      }
+      else
+      {
+         cout<<2;
+         fr(i,n-1)
+         cout<<3;
+         cout<<"\n";
+      }
+   }
+   int main()
+   {
+      ios::sync_with_stdio(0);
+      cin.tie(0);
+      int t;
+      cin>>t;
+      while(t--)
+         solve();
+      return 0;    
+   }
