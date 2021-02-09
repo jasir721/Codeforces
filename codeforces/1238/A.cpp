a9f5135fdaf81fcc67b00621def6aa33f923ac45
Jasir K <jasirkadarsha721091@gmail.com>
2021-02-09 22:12:00 +0530

    Add solution for problem `A - Prime Subtraction` Link: https://codeforces.com/contest/1238/problem/A Tags: math, number theory, *900 Ref: https://codeforces.com/contest/1238/submission/107003289

---
+++ codeforces/1238/A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fr(i,n) for(int i=0;i<n;i++)
+int gcd(int a,int b) { if (a == 0) return b; return gcd(b%a, a);}
+void solve()
+{   
+   ll n,m;
+   cin>>n>>m;
+   if(n-m>1)
+   cout<<"YES\n";
+   else 
+   cout<<"NO\n";
+}
+int main()
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   int t;
+   cin>>t;
+   while(t--)
+      solve();
+   return 0;    
+}
