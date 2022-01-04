126a3a667c1e908760194e759548108f962cf8bd
Jasir K <jasirkadarsha721091@gmail.com>
2022-01-04 23:52:00 +0530

    Add solution for problem `A - Blackjack` Link: https://codeforces.com/contest/104/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/104/submission/141673203

---
+++ codeforces/104/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fr(i,n) for(int i=0;i<n;i++)
+int gcd(int a,int b) { if (a == 0) return b; return gcd(b%a, a);}
+void solve()
+{   
+    int n;
+    cin>>n;
+    if ( n<=10 or n>21 ) cout<<0;
+    else if (n-10==10) cout<<15;
+    else cout<<4;
+}
+int main()
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+//    int t;
+//    cin>>t;
+//    while(t--)
+      solve();
+   return 0;    
+}
