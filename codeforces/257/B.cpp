9af0f5f2be49c3d023fbc26b1cc7ef0cc0ce3aab
Jasir K <jasirkadarsha721091@gmail.com>
2022-01-13 17:42:00 +0530

    Add solution for problem `B - Playing Cubes` Link: https://codeforces.com/contest/257/problem/B Tags: games, greedy, implementation, *1300 Ref: https://codeforces.com/contest/257/submission/142603529

---
+++ codeforces/257/B.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fr(i,n) for(int i=0;i<n;i++)
+int gcd(int a,int b) { if (a == 0) return b; return gcd(b%a, a);}
+void solve()
+{   
+	int a, b; cin >> a >> b;
+	cout << max(a, b) - 1 << " " << min(a, b);
+
+}
+int main()
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+//    int t;
+//    cin>>t;
+//    while(t--)
+      solve();
+   return 0;    
+}
