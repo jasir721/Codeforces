e2ac5a68d6b1570492c54fc7905215c4dd386c60
Jasir K <jasirkadarsha721091@gmail.com>
2022-01-18 14:21:00 +0530

    Add solution for problem `B - Hungry Sequence` Link: https://codeforces.com/contest/327/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/327/submission/143201936

---
+++ codeforces/327/B.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fr(i,n) for(int i=0;i<n;i++)
+int gcd(int a,int b) { if (a == 0) return b; return gcd(b%a, a);}
+void solve()
+{   
+   int n;
+   cin>>n;
+   fr(i,n)
+   {
+       cout<<(3*n)+i<<" ";
+   }
+   return;
+}
+int main()
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+//    int t;
+//    cin>>t;
+//    while(t--)
+      solve();
+   return 0;    
+}
